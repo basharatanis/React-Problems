d8e7a403fddcf3060d763ae17c8a8594b94987ab
basharatanis <56668352+basharatanis@users.noreply.github.com>
2020-05-20 23:50:02 -0700

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,37 @@
+MacOS:
+Install Homebrew by running
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" in the terminal.
+
+Check that it was installed by running brew --version. You should see the version number that was installed.
+
+Run brew install node.
+
+Run node --version.
+
+Check that npm was installed as well by running npm --version.
+
+Run brew install yarn.
+
+Run npm --version.
+
+Run yarn install && yarn --version
+
+
+
+
+Windows:
+
+Please download the Node.js Installer, go through the installation process, and restart your computer once you're done.
+Please follow the yarn installation instructions.
+Run yarn --version to make sure yarn has been successfully installed.
+
+
+
+
+Linux
+
+Please follow these instructions to install Node.js.
+Run sudo apt-get install -y build-essential.
+Please follow the yarn installation instructions.
+Run yarn --version to make sure yarn has been successfully installed.
